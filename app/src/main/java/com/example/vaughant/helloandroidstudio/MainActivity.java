6ad2162f7f13010e305ec36e01a860edb4df52bd
bluespacecube <bluespacecube@gmail.com>
2018-02-01 22:58:23 +0000

    added custom message dialog

--- app/src/main/java/com/example/vaughant/helloandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/vaughant/helloandroidstudio/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.vaughant.helloandroidstudio;
 
+import android.app.AlertDialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                TextView f = new TextView(getApplicationContext());
+                LinearLayout linlay = findViewById(R.id.items_linLay);
                 Snackbar.make(view, "Hi!", Snackbar.LENGTH_SHORT).show();
                 f.setText("Dynamically added text view "+ dynTvCounter);
                 dynTvCounter += 1;
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // maybe make a library or something (for dialogs, etc...)
+    // figure out unit testing
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -73,5 +82,37 @@ public class MainActivity extends AppCompatActivity {
         TestBaseClass testBaseClass = new TestBaseClass();
         TextView tv = findViewById(R.id.myTextView);
         testBaseClass.doSomething(tv);
+    }
+
+    public void testDialogBtnClick(View view) {
+        Sapphire("Player ??? Wins!", "OPps?", null,true, true);
+    }
+    public void Sapphire(String msg, String btnmsg, Drawable img, Boolean cancelable, Boolean cancelableany) {
+        View deleteDialogView = LayoutInflater.from(this).inflate(R.layout.cust_popup, null);
+        final AlertDialog deleteDialog2 = new AlertDialog.Builder(this).create();
+        deleteDialog2.setView(deleteDialogView);
+        deleteDialogView.findViewById(R.id.saph_btn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                deleteDialog2.dismiss();
+            }
+        });
+        if (cancelableany.booleanValue()) {
+            deleteDialogView.findViewById(R.id.saph_linlay).setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    deleteDialog2.dismiss();
+                }
+            });
+        }
+        ((Button) deleteDialogView.findViewById(R.id.saph_btn)).setText(btnmsg);
+        ImageView op = (ImageView) deleteDialogView.findViewById(R.id.saph_imgview);
+        ((TextView) deleteDialogView.findViewById(R.id.saph_txt)).setText(msg);
+        if (img != null) {
+            op.setImageDrawable(img);
+        }
+        Window window = deleteDialog2.getWindow();
+        window.setLayout(-2, -2);
+        window.setGravity(17);
+        deleteDialog2.setCancelable(cancelable.booleanValue());
+        deleteDialog2.show();
     }
 }
